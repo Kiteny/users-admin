4fe3e8d06d9a0cecdc09b1ca8440ed3e3c96dfac
Artyom <bitcoinfeks@gmail.com>
2020-12-11 16:12:08 +0500

    Пробросил свойства name и id в Input

--- src/ui/Input/Input.jsx
+++ src/ui/Input/Input.jsx
@@ -1,27 +1,39 @@
-import React from 'react';
+import React, { forwardRef } from 'react';
 import PropTypes from 'prop-types';
 
 import styles from './Input.module.css';
 
-const Input = ({ title, type }) => {
+const Input = forwardRef(({
+  title, type, name, id,
+}, ref) => {
   const rendredTitle = title ? `${title}:` : '';
 
   return (
     <div>
       <p className={styles.title}>{rendredTitle}</p>
-      <input className={styles.input} type={type} />
+      <input
+        className={styles.input}
+        type={type}
+        ref={ref}
+        name={name}
+        id={id}
+      />
     </div>
   );
-};
+});
 
 Input.propTypes = {
   title: PropTypes.string,
   type: PropTypes.string,
+  name: PropTypes.string,
+  id: PropTypes.string,
 };
 
 Input.defaultProps = {
   title: '',
   type: 'text',
+  name: '',
+  id: '',
 };
 
 export default Input;
