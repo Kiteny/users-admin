800f1f8638c7145507414c4d5005fc5d6903ea75
Artyom <bitcoinfeks@gmail.com>
2020-12-11 09:42:00 +0500

    Доработал UsersControlPanel

--- src/features/users/UsersControlPanel/UsersControlPanel.jsx
+++ src/features/users/UsersControlPanel/UsersControlPanel.jsx
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { MdPersonAdd } from 'react-icons/md';
 
 import UsersList, { UsersListItem } from '../UsersList';
+import TopBar from '../TopBar';
+import DropDown from '../../../ui/DropDown';
 import styles from './UsersControlPanel.module.css';
 
 const UsersControlPanel = () => {
+  const [selectedRole, setSelectedRole] = useState('Все');
+
   const headers = [
     'ФИО', 'Email', 'Пароль',
     'Телефон', 'Роль', 'Дата регистрации',
@@ -27,33 +32,181 @@ const UsersControlPanel = () => {
       email: 'dfdsfd@gmail.com',
       password: 'qwerty',
       phone: '+77777777777',
-      role: 'Client',
+      role: 'Админ',
       dateReg: '23.12.2229',
       dateLastEdit: '23.12.2229',
     },
     {
       id: '3',
-      fio: 'Иванов2 Ивна Иванович',
+      fio: 'Иванов2121 Ивна Иванович',
       email: 'dfdsfd@gmail.com',
       password: 'qwertydsfsdfdsfsdfdsfds',
       phone: '+77777777777',
-      role: 'Client',
+      role: 'Клиент',
       dateReg: '23.12.2229',
       dateLastEdit: '23.12.2229',
     },
     {
       id: '4',
-      fio: 'Иванов2 Ивна Иванович',
+      fio: 'Иванов2 Ивна ddsd',
       email: 'dfdsfd@gmail.com',
       password: 'qwerty',
       phone: '+77777777777',
-      role: 'Client',
+      role: 'Клиент',
+      dateReg: '23.12.2229',
+      dateLastEdit: '23.12.2229',
+    },
+    {
+      id: '5',
+      fio: 'Иванов2 Ивна ddsd',
+      email: 'dfdsfd@gmail.com',
+      password: 'qwerty',
+      phone: '+77777777777',
+      role: 'Клиент',
+      dateReg: '23.12.2229',
+      dateLastEdit: '23.12.2229',
+    },
+    {
+      id: '6',
+      fio: 'Иванов2 Ивна ddsd',
+      email: 'dfdsfd@gmail.com',
+      password: 'qwerty',
+      phone: '+77777777777',
+      role: 'Клиент',
+      dateReg: '23.12.2229',
+      dateLastEdit: '23.12.2229',
+    },
+    {
+      id: '7',
+      fio: 'Иванов2 Ивна ddsd',
+      email: 'dfdsfd@gmail.com',
+      password: 'qwerty',
+      phone: '+77777777777',
+      role: 'Клиент',
+      dateReg: '23.12.2229',
+      dateLastEdit: '23.12.2229',
+    },
+    {
+      id: '8',
+      fio: 'Иванов2 Ивна ddsd',
+      email: 'dfdsfd@gmail.com',
+      password: 'qwerty',
+      phone: '+77777777777',
+      role: 'Клиент',
+      dateReg: '23.12.2229',
+      dateLastEdit: '23.12.2229',
+    },
+    {
+      id: '9',
+      fio: 'Иванов2 Ивна ddsd',
+      email: 'dfdsfd@gmail.com',
+      password: 'qwerty',
+      phone: '+77777777777',
+      role: 'Клиент',
+      dateReg: '23.12.2229',
+      dateLastEdit: '23.12.2229',
+    },
+    {
+      id: '10',
+      fio: 'Иванов2 Ивна ddsd',
+      email: 'dfdsfd@gmail.com',
+      password: 'qwerty',
+      phone: '+77777777777',
+      role: 'Клиент',
+      dateReg: '23.12.2229',
+      dateLastEdit: '23.12.2229',
+    },
+    {
+      id: '11',
+      fio: 'Иванов2 Ивна ddsd',
+      email: 'dfdsfd@gmail.com',
+      password: 'qwerty',
+      phone: '+77777777777',
+      role: 'Клиент',
+      dateReg: '23.12.2229',
+      dateLastEdit: '23.12.2229',
+    },
+    {
+      id: '12',
+      fio: 'Иванов2 Ивна ddsd',
+      email: 'dfdsfd@gmail.com',
+      password: 'qwerty',
+      phone: '+77777777777',
+      role: 'Клиент',
+      dateReg: '23.12.2229',
+      dateLastEdit: '23.12.2229',
+    },
+    {
+      id: '13',
+      fio: 'Иванов2 Ивна ddsd',
+      email: 'dfdsfd@gmail.com',
+      password: 'qwerty',
+      phone: '+77777777777',
+      role: 'Клиент',
+      dateReg: '23.12.2229',
+      dateLastEdit: '23.12.2229',
+    },
+    {
+      id: '14',
+      fio: 'Иванов2 Ивна ddsd',
+      email: 'dfdsfd@gmail.com',
+      password: 'qwerty',
+      phone: '+77777777777',
+      role: 'Клиент',
+      dateReg: '23.12.2229',
+      dateLastEdit: '23.12.2229',
+    },
+    {
+      id: '15',
+      fio: 'Иванов2 Ивна ddsd',
+      email: 'dfdsfd@gmail.com',
+      password: 'qwerty',
+      phone: '+77777777777',
+      role: 'Клиент',
+      dateReg: '23.12.2229',
+      dateLastEdit: '23.12.2229',
+    },
+    {
+      id: '16',
+      fio: 'Иванов2 Ивна ddsd',
+      email: 'dfdsfd@gmail.com',
+      password: 'qwerty',
+      phone: '+77777777777',
+      role: 'Клиент',
+      dateReg: '23.12.2229',
+      dateLastEdit: '23.12.2229',
+    },
+    {
+      id: '17',
+      fio: 'Иванов2 Ивна ddsd',
+      email: 'dfdsfd@gmail.com',
+      password: 'qwerty',
+      phone: '+77777777777',
+      role: 'Клиент',
+      dateReg: '23.12.2229',
+      dateLastEdit: '23.12.2229',
+    },
+    {
+      id: '18',
+      fio: 'Иванов2 Ивна ddsd',
+      email: 'dfdsfd@gmail.com',
+      password: 'qwerty',
+      phone: '+77777777777',
+      role: 'Клиент',
       dateReg: '23.12.2229',
       dateLastEdit: '23.12.2229',
     },
   ];
 
-  const renderedUsers = users.map((user) => (
+  const roles = ['Все', 'Клиент', 'Партнёр', 'Админ'];
+
+  const filterdUsers = users.filter((user) => user.role === selectedRole || selectedRole === 'Все');
+
+  const handlerRoleChange = (id) => {
+    setSelectedRole(roles[id]);
+  };
+
+  const renderedUsers = filterdUsers.map((user) => (
     <UsersListItem
       key={user.id}
       {...user}
@@ -62,6 +215,12 @@ const UsersControlPanel = () => {
 
   return (
     <div className={styles.container}>
+      <TopBar>
+        <DropDown items={roles} onChange={handlerRoleChange} />
+        <button type="button">
+          <MdPersonAdd size="28" />
+        </button>
+      </TopBar>
       <UsersList headers={headers}>
         {renderedUsers}
       </UsersList>
