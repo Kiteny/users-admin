c8c4a6b108248f8cbabea60047a58c592ed74114
Artyom <bitcoinfeks@gmail.com>
2020-12-11 13:57:50 +0500

    Доработал Button

--- src/ui/Button/Button.jsx
+++ src/ui/Button/Button.jsx
@@ -3,11 +3,15 @@ import PropTypes from 'prop-types';
 
 import styles from './Button.module.css';
 
-const Button = ({ title, icon }) => {
+const Button = ({
+  title, icon, fulfilled, type, className,
+}) => {
   const buttonStyles = icon ? styles.buttonWithIcon : styles.button;
+  const fulfilledStyles = fulfilled ? styles.fulfilled : '';
 
   return (
-    <button type="button" className={buttonStyles}>
+    // eslint-disable-next-line react/button-has-type
+    <button type={type} className={`${buttonStyles} ${fulfilledStyles} ${className}`}>
       <span className={styles.icon}>{icon}</span>
       {title}
     </button>
@@ -17,11 +21,17 @@ const Button = ({ title, icon }) => {
 Button.propTypes = {
   title: PropTypes.string,
   icon: PropTypes.node,
+  fulfilled: PropTypes.bool,
+  type: PropTypes.string,
+  className: PropTypes.string,
 };
 
 Button.defaultProps = {
   title: '',
   icon: null,
+  fulfilled: false,
+  type: 'button',
+  className: '',
 };
 
 export default Button;
