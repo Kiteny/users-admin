3519593d6231492c128c03a4373860cdd47e575f
Artyom <bitcoinfeks@gmail.com>
2020-12-11 15:15:03 +0500

    Пробросил атрибу type в Input

--- src/ui/Input/Input.jsx
+++ src/ui/Input/Input.jsx
@@ -3,23 +3,25 @@ import PropTypes from 'prop-types';
 
 import styles from './Input.module.css';
 
-const Input = ({ title }) => {
+const Input = ({ title, type }) => {
   const rendredTitle = title ? `${title}:` : '';
 
   return (
     <div>
       <p className={styles.title}>{rendredTitle}</p>
-      <input className={styles.input} />
+      <input className={styles.input} type={type} />
     </div>
   );
 };
 
 Input.propTypes = {
   title: PropTypes.string,
+  type: PropTypes.string,
 };
 
 Input.defaultProps = {
   title: '',
+  type: 'text',
 };
 
 export default Input;
